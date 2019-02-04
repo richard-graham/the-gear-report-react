47a12f14a5445b262470ea1b1ce8ea8c9131ba72
Richard Graham <richgrahamdev@gmail.com>
2019-02-04 21:56:55 +1300

    file init

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,48 @@
 import React from 'react'
+import { HashRouter as Router, Route } from 'react-router-dom'
 
-const App = () => {
-  return (
-    <h1>React development has begun!</h1>
-  )
+import islands from "../../data/01_islands"
+
+const Region = (props)=>{
+  return <div>{props.regions.map((line)=>{ return <p>{line.name}</p>})}</div>
+}
+
+const Stuff = (props)=>{
+  return <h1>Stuff</h1>
+}
+
+// const App = () => {
+class App extends React.Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      data: [],
+      level: "region"
+    }
+  }
+
+  componentDidMount(){
+
+    this.setState({
+      data: islands
+    })
+
+
+  }
+
+  render(){
+    return (
+      <div>
+        <h1>React development has begun!</h1>
+
+        <Region regions={this.state.data}/>
+        <Router>
+          <Route path="/region" exact component={Stuff} />
+        </Router>
+      </div>
+    )
+  }
 }
 
 export default App
--- server/server.js
+++ server/server.js
@@ -1,9 +1,17 @@
 const path = require('path')
 const express = require('express')
 
+
 const server = express()
 
 server.use(express.json())
 server.use(express.static(path.join(__dirname, './public')))
 
+
+server.get("/stuff",(req,res)=>{
+    res.send("information")
+})
+
 module.exports = server
+
+
