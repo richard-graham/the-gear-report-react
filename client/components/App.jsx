83f8dde7255be3e5c59f7ac9a85803322c99cee1
Richard Graham <richgrahamdev@gmail.com>
2019-02-05 10:03:32 +1300

    Island comp linking

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,49 +1,68 @@
+// import React from 'react'
+// import { HashRouter as Router, Route } from 'react-router-dom'
+
+// import islands from "../../data/01_islands"
+
+// const Region = (props)=>{
+//   return <div>{props.regions.map((line)=>{ return <p>{line.name}</p>})}</div>
+// }
+
+// const Stuff = (props)=>{
+//   return <h1>Stuff</h1>
+// }
+
+// // const App = () => {
+// class App extends React.Component {
+//   constructor(props){
+//     super(props)
+
+//     this.state = {
+//       data: [],
+//       level: "region"
+//     }
+//   }
+
+//   componentDidMount(){
+
+//     this.setState({
+//       data: islands
+//     })
+
+
+//   }
+
+//   render(){
+//     return (
+//       <div>
+//         <h1>React development has begun!</h1>
+
+//         <Region regions={this.state.data}/>
+//         <Router>
+//           <Route path="/region" exact component={Stuff} />
+//         </Router>
+//       </div>
+//     )
+//   }
+// }
+
+// export default App
+
 import React from 'react'
 import { HashRouter as Router, Route } from 'react-router-dom'
 
-import islands from "../../data/01_islands"
+import Nav from './Nav'
+import Header from './Header'
+import Island from './Island'
 
-const Region = (props)=>{
-  return <div>{props.regions.map((line)=>{ return <p>{line.name}</p>})}</div>
-}
-
-const Stuff = (props)=>{
-  return <h1>Stuff</h1>
-}
-
-// const App = () => {
-class App extends React.Component {
-  constructor(props){
-    super(props)
-
-    this.state = {
-      data: [],
-      level: "region"
-    }
-  }
-
-  componentDidMount(){
-
-    this.setState({
-      data: islands
-    })
-
-
-  }
-
-  render(){
-    return (
+const App = () => {
+  return (
+    <Router>
       <div>
-        <h1>React development has begun!</h1>
-
-        <Region regions={this.state.data}/>
-        <Router>
-          <Route path="/region" exact component={Stuff} />
-        </Router>
+        <Route path='/' component={Island} />
       </div>
-    )
-  }
+     
+    </Router>
+  )
 }
 
 export default App
-
